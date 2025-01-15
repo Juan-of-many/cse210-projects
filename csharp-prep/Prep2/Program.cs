ed4f8625ac1e49abc6a09546b6a683586161bf7e
Juan-of-many <fiery.fynx@gmail.com>
2025-01-15 04:45:06 -0900

    W01 Prep2 comment skeleton

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //get user input of grade
+        Console.WriteLine("Hi there! What is your grade percentage? ");
+        
+
+        //convert string to int
+
+
+        //compare numerical grade to letter-grade on Grading Scale found below.
+        //convert numerical grade to appropriate letter-grade. 
+        
+        
+        /*
+        Grading Scale:
+        A >= 90
+        B >= 80
+        C >= 70
+        D >= 60
+        F < 60
+        */
     }
 }
