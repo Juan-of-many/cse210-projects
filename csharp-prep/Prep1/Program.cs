23848717887cdea5637e605a3f3ce32067b88bdf
Juan-of-many <fiery.fynx@gmail.com>
2025-01-15 04:00:47 -0900

    W01 Prep1 activity dealing with simple print to screen using  user input. End product is James Bond introduction.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
+//Hi, I am a test note/comment
+/*me
+too */
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //get user input for first and last name.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        
+        //print user input results in fancy James Bond format.
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
