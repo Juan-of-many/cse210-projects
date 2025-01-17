60fcab8e7ca3ce461f93859ce288b62b694b5cdf
Juan-of-many <fiery.fynx@gmail.com>
2025-01-16 21:32:48 -0900

    w02 Prep5 practicing with functions full

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+    
+    static void DisplayWelcome()
+        {
+            // function_body
+            Console.WriteLine("Welcome to the program!");
+        }
+
+    static string PromptUserName()
+        {
+            //function_body
+            Console.Write("Please enter your name or pseudonym: ");
+            string localUserName = Console.ReadLine();
+
+            return localUserName;
+        }
+    
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string localUserChoice = Console.ReadLine();
+        int localUserNumber = int.Parse(localUserChoice);
+        
+        return localUserNumber;
+    }
+
+    static int SquareNumber(int num)
+    {
+        int squared = num * num;
+
+        return squared;
+    }
+
+    static void DisplayResult(string userName, int squaredNum)
+    {
+        Console.Write($"{userName}, the square of your number is {squaredNum}");
     }
 }
