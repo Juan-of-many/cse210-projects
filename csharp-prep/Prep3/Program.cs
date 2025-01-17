14eefc0c6d57dbd3d8c7e39b0cede58d894de1a1
Juan-of-many <fiery.fynx@gmail.com>
2025-01-16 19:59:40 -0900

    w02 Prep3 random number hi-low game full

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -13,7 +13,7 @@ class Program
         string tutorialOptIn = tutorialQuestion.ToLower();
         if (tutorialOptIn == "y" || tutorialOptIn == "ye" || tutorialOptIn == "yes" || tutorialOptIn == "yess" || tutorialOptIn == "sure" || tutorialOptIn == "1")
         {
-            Console.WriteLine("First, I will choose a number.");
+            Console.WriteLine("First, I will choose a number from 1-99.");
             Console.WriteLine("Then, you will try to guess my number.");
             Console.WriteLine("I will give you hints with each guess, telling you whether my number is higher or lower than your current guess.");
         }
@@ -23,13 +23,66 @@ class Program
         while (play == "yes")
         {
             //pick random number
+            Random randomNumGenerator = new Random();
+            int randomNum = randomNumGenerator.Next(1,99);
 
-            //open another loop
-            //player guesses
-            //give hints
+            Console.WriteLine($"{randomNum}");
 
+            //create guess variable
+            int guess = 0;
+            int guessAmount = 0;
 
-            //ask if play again
+            //open another loop for guesses to happen inside
+            do
+            {
+                //player guesses
+                Console.Write("What is your guess? ");
+                string guessUser = Console.ReadLine();
+
+                //convert string to int
+                guess = int.Parse(guessUser);
+
+                //guess counter
+                guessAmount += 1;
+
+                //take guess and compare to our random number choice from before.
+                //give hint
+                if (guess > randomNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < randomNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Congrats! You guessed it!");
+                }
+
+            } while (guess != randomNum);
+
+            if (guessAmount == 1)
+            {
+                Console.WriteLine("You guessed right on your very first try!");
+            }
+            else
+            {
+               Console.WriteLine($"It only took you {guessAmount} guesses!"); 
+            }
+            
+
+            //ask if play again to escape the loop
+            Console.WriteLine();
+            Console.Write("Would you like to continue playing? ");
+            string playChoiceUser = Console.ReadLine();
+
+            //set to lowercase to check answer. If answer doesn't include any of the affirmative responses, set play to 'no'.
+            string playChoice = playChoiceUser.ToLower();
+            if (!(playChoice == "y" || playChoice == "ye" || playChoice == "yes" || playChoice == "yess" || playChoice == "sure" || playChoice == "1"))
+            {
+                play = "no";
+            }
         }
     }
 }
