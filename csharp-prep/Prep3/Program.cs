b4eb957b0be045ade5703fdd896a63805afde3fe
Juan-of-many <fiery.fynx@gmail.com>
2025-01-16 19:02:45 -0900

    w02 Prep3 random number hi-low game skeleton

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //introduce to game, tutorial can be opted in or out.
+        Console.WriteLine("Hi there! Let's play a game called hi-low!");
+        Console.Write("Would you like a quick explanation on how to play? ");
+        string tutorialQuestion = Console.ReadLine();
+        
+        //tutorial explanation of how to play
+        string tutorialOptIn = tutorialQuestion.ToLower();
+        if (tutorialOptIn == "y" || tutorialOptIn == "ye" || tutorialOptIn == "yes" || tutorialOptIn == "yess" || tutorialOptIn == "sure" || tutorialOptIn == "1")
+        {
+            Console.WriteLine("First, I will choose a number.");
+            Console.WriteLine("Then, you will try to guess my number.");
+            Console.WriteLine("I will give you hints with each guess, telling you whether my number is higher or lower than your current guess.");
+        }
+        
+        //gameplay loop
+        string play = "yes";
+        while (play == "yes")
+        {
+            //pick random number
+
+            //open another loop
+            //player guesses
+            //give hints
+
+
+            //ask if play again
+        }
     }
 }
