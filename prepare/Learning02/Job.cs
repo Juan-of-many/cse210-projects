4081fc526ce53421e92487473937daf5afb3d227
Juan-of-many <fiery.fynx@gmail.com>
2025-01-23 18:47:46 -0900

    w03 job and resume class creation. Just the job class part of the assignment

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear; //not yet assigned a value, but told it is meant to be an int... I do not know if this fully works.
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+}
+
+//behavior 
+//Displays the job information in the format "Job Title (Company) StartYear-EndYear", for example: "Software Engineer (Microsoft) 2019-2022".
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job(); //create new job instance named job1
+        
+        //mini test to see if Job class works.
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 1;
+        job1._endYear = 4;
+
+        Job job2 = new Job(); //create a second job
+
+        //set second job's variables
+        job2._company = "Apple";
+        job2._jobTitle = "Hiring Manager";
+        job2._startYear = 1967;
+        job2._endYear = 1988;
+
+        //display
+        job1.Display();
+        job2.Display();
     }
 }
