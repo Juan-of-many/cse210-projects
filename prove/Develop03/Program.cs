ff41036613d66f3775aef55e0e316852638975cd
Juan-of-many <fiery.fynx@gmail.com>
2025-02-17 17:15:23 -0900

    w06 finalization

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,11 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        
         //setting variables here
         string play = "yes";
         string choice;
-        bool wordless = false;
 
         //create an instance of proper class or classes
         Scripture scriptureInstance = new Scripture();
@@ -16,14 +14,29 @@ class Program
         //choose the scripture being memorized
         scriptureInstance.GenerateSelection();
 
+        //turn the selection into something usable with my Display function, which calls the compiler to get the scripture text.
+        scriptureInstance.PhraseIntoWord();
+
         //clear screen
         Console.Clear();
 
         //print full, unchanged scripture
-        scriptureInstance.Display(); //may need to fix
+        scriptureInstance.Display();
+
+        //determine choice, asking user if they wish to continue
+        choice = GetUserChoice();
+            
+        //if condition met, exit
+        if (choice == "quit")
+        {
+            play = "no";
+        }
         
-        //open a loop that continues until quit
-        do
+        //set the index to match starting scripture, needing PhraseIntoWord above it to set formatting properly
+        scriptureInstance.SetStartingIndex();
+        
+        //open a loop that continues until quit or empty
+        while (play == "yes")
         {
             //alter scripture x times, in this case 3 words are removed
             int x = 3;
@@ -40,41 +53,51 @@ class Program
             //print scripture in current form
             scriptureInstance.Display();
 
+            choice = GetUserChoice();
+
             //check if scripture is wordless
             if (scriptureInstance.IsTextIndexEmpty() == true)
-            {
-                wordless = true;
-            }
-            //else, it has data and it stays as default of false
-
-            //prompt user to continue, with polite whitespace for formatting
-            Console.WriteLine();
-            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
-            string userChoice = Console.ReadLine();
-
-            //fix formatting of userChoice
-            string lowerChoice = userChoice.ToLower();
-
-            //determine if quit
-            if (lowerChoice == "q" || lowerChoice == "qq" || lowerChoice == "quit" || lowerChoice == "0")
-            {
+            {   
+                //wordless is true, so choice = "quit", regardless of what user chose above.
+                //wordless = true;
                 choice = "quit";
             }
-            else if (wordless == true)
-            {
-                choice = "quit";
-            }
-            else
-            {
-                choice = "continue"; 
-            }
-            
-            //if condition met, exit
+            //else, it has data and it stays as default of false or user's choice.
+
+            //if exit condition of choice being quit or wordless being true met, exit
             if (choice == "quit")
             {
                 play = "no";
             }
-        } while (play == "yes");
+        }
         
+    }
+
+
+    //simplify code by splitting into functions
+    static string GetUserChoice()
+    {
+        //set return variable
+        string choice;
+
+        //prompt user to continue, with polite whitespace for formatting
+        Console.WriteLine();
+        Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+        string userChoice = Console.ReadLine();
+
+        //fix formatting of userChoice
+        string lowerChoice = userChoice.ToLower();
+
+        //determine if quit
+        if (lowerChoice == "q" || lowerChoice == "qq" || lowerChoice == "quit" || lowerChoice == "0")
+        {
+            choice = "quit";
+        }
+        else
+        {
+            choice = "continue"; 
+        }
+
+        return choice;
     }
 }
